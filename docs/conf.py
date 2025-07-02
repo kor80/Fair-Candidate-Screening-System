a36884a4d29e90041e3310c10a4cac531a906062
tommaso perniola <79906253+t-perniola@users.noreply.github.com>
2025-07-02 23:45:12 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,13 @@ project = 'Fair-Candidate-Screening-System'
 author = 'CAT'
 release = '0.1'
 
-extensions = []
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',  # optional, for math support
+]
+
+# Tell nbsphinx to allow execution or not (optional)
+nbsphinx_execute = 'never'  # or 'auto' if you want to run notebooks on build
 
 templates_path = ['_templates']
 exclude_patterns = []
