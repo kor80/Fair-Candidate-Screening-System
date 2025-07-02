63f6e51c891477b7fa04b212410e45daebd9458a
tommaso perniola <79906253+t-perniola@users.noreply.github.com>
2025-07-02 20:51:49 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'Fair-Candidate-Screening-System'
+author = 'CAT'
+release = '0.1'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
